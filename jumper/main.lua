aa4553ec0cf38f14b972069e0692b031fba822a2
phopps <pathopps1@gmail.com>
2022-12-17 13:53:05 -0500

    updated game title to jumper and formatted code

--- jumper/main.lua
+++ jumper/main.lua
@@ -1,8 +1,5 @@
-
-
 function love.load()
-
-  love.window.setTitle("Some Game Thing")
+  love.window.setTitle("Jumper")
 
   player = {}
   player.x = 400
@@ -25,16 +22,14 @@ function love.update(dt)
 end
 
 function love.draw()
-
   drawPlayer()
   drawPlatforms()
 end
 
 function love.keypressed(key, scancode, isrepeat)
-  
 end
 
-function love.mousepressed( x, y, button, istouch, presses )
+function love.mousepressed(x, y, button, istouch, presses)
   --print("clicked the mouse.")
   if player.touch_ground == true then
     player.y_velocity = -10
@@ -53,19 +48,18 @@ function updatePlayer(dt)
 end
 
 function drawPlayer()
-
-  love.graphics.setColor(1,0,0,1)
-  love.graphics.rectangle("fill",player.x, player.y, player.width, player.height)
+  love.graphics.setColor(1, 0, 0, 1)
+  love.graphics.rectangle("fill", player.x, player.y, player.width, player.height)
 end
 
 function drawPlatforms()
-  love.graphics.setColor(0,1,0,1)
-  for i,plat in ipairs(platforms) do
-    love.graphics.rectangle("fill",plat.x, plat.y, plat.width, plat.height)
+  love.graphics.setColor(0, 1, 0, 1)
+  for i, plat in ipairs(platforms) do
+    love.graphics.rectangle("fill", plat.x, plat.y, plat.width, plat.height)
   end
 end
 
-function createPlatform(x,y,width,height)
+function createPlatform(x, y, width, height)
   local p = {}
   p.x = x
   p.y = y
@@ -73,7 +67,6 @@ function createPlatform(x,y,width,height)
   p.height = height
 
   table.insert(platforms, p)
-
 end
 
 function checkCollisions()
@@ -81,32 +74,33 @@ function checkCollisions()
 
   local player_left, player_right, player_top, player_bottom
   player_left = player.x
-  player_right = player.x+player.width
+  player_right = player.x + player.width
   player_top = player.y
-  player_bottom = player.y+player.height
+  player_bottom = player.y + player.height
 
-  for i,plat in ipairs(platforms) do
+  for i, plat in ipairs(platforms) do
     -- does this collide with the player?
     local plat_left, plat_right, plat_top, plat_bottom
     plat_left = plat.x
-    plat_right = plat.x+plat.width
+    plat_right = plat.x + plat.width
     plat_top = plat.y
-    plat_bottom = plat.y+plat.height
+    plat_bottom = plat.y + plat.height
 
     if plat_left < player_right and plat_right > player_left and plat_top < player_bottom and plat_bottom > player_top then
       print("COLLIDE")
-    elseif plat_left < player_right and plat_right > player_left and plat_top < player_bottom+10 and plat_bottom > player_top then
+    elseif
+      plat_left < player_right and plat_right > player_left and plat_top < player_bottom + 10 and
+        plat_bottom > player_top
+     then
       --print("feet touch")
       player.touch_ground = true
     -- else
     --   player.touch_ground = false
     end
-
   end
 
   --extra step to check player feet?
 end
 
 function is_player_on_ground()
-
 end
